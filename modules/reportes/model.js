8ee5060af4187fc0e8cf826068a381605c2e9599
nachibat <nachi2083@gmail.com>
2024-10-08 19:22:06 -0300

    Se agrega reporte de mas vendidos

--- modules/reportes/model.js
+++ modules/reportes/model.js
@@ -1,15 +1,17 @@
 const queryMySQL = require('../../config/database').queryMySQL;
 
 exports.getMasVendidos = () => {
-    return queryMySQL(`SELECT a.id, 
-                            a.img, 
-                            a.descripcion, 
-                            a.precio, 
-                            a.iva, 
-                            COUNT(pd.articulo) AS veces_vendido
-                        FROM pedidos_detalles pd
-                        JOIN articulos a ON pd.articulo = a.id
-                        GROUP BY a.id, a.descripcion, a.precio
-                        ORDER BY veces_vendido DESC
-                        LIMIT 10;`, []);
+    return queryMySQL(`SELECT articulos.id, 
+                        articulos.img, 
+                        articulos.descripcion as articulo, 
+                        articulos.precio, 
+                        articulos.iva,
+                        rubros.descripcion as rubro, 
+                        COUNT(pedidos_detalles.articulo) AS veces_vendido
+                    FROM pedidos_detalles, articulos, rubros
+                    WHERE pedidos_detalles.articulo = articulos.id
+                    AND articulos.id_rubro = rubros.id
+                    GROUP BY articulos.id, articulos.img, articulos.descripcion, articulos.precio, articulos.iva, rubros.descripcion
+                    ORDER BY veces_vendido DESC
+                    LIMIT 10;`, []);
 }
